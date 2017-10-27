cd66424ed415a590ae87919cda180d9ff30ea846
colin <lilonglongfwz@gmail.com>
2017-10-27 23:28:09 +0800

    Revert "[rails] generate the project"
    This reverts commit 57d8a8538d7bd6a56ed9f7cbc4d7a8e4fad9ed1d.

--- company_index/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-CompanyIndex::Application.config.secret_token = 'f212764725c2e9400961a50867bacf8066fc9cdb1d6d62d3c5122a9ef34512c3b10263de315475a809319ce67672d1c02b686d2548ab544949b807dad086d5c0'
